85dc20029d3f56cd1e23ab14d381f0d5c7688af4
Kailerhighenddetailing <crystal.1981@hotmail.com>
2026-06-25 18:30:07 -0600

    Update quote.js

--- quote.js
+++ quote.js
@@ -1,1 +1,34 @@
+const startBtn=document.getElementById("startBtn");
 
+const welcomeScreen=document.getElementById("welcomeScreen");
+
+const wizard=document.getElementById("wizard");
+
+startBtn.addEventListener("click",(e)=>{
+
+e.preventDefault();
+
+welcomeScreen.style.display="none";
+
+wizard.style.display="block";
+
+window.scrollTo({
+top:0,
+behavior:"smooth"
+});
+
+});
+
+const vehicleCards=document.querySelectorAll(".vehicle-option");
+
+vehicleCards.forEach(card=>{
+
+card.addEventListener("click",()=>{
+
+vehicleCards.forEach(c=>c.classList.remove("selected"));
+
+card.classList.add("selected");
+
+});
+
+});
